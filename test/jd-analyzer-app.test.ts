79cfbd607ad3af4154782ec8530ad0166a9ab379
Dante Alonso <dantealonsoh@Dantes-MacBook-Pro.local>
2024-08-06 14:32:40 -0600

    Initial commit

---
+++ test/jd-analyzer-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JdAnalyzerApp from '../lib/jd-analyzer-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jd-analyzer-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JdAnalyzerApp.JdAnalyzerAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
